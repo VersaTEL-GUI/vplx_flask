9e90291bb3d471690d5f92a3951ae1b832255461
Ethan <ethan.lv@feixitek.com>
2020-09-10 13:53:09 +0800

    test, ok

--- manage.py
+++ manage.py
@@ -4,21 +4,17 @@ from flask import Flask, jsonify
 import subprocess
 
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 class Config(object):
     DEBUG = True
 
-    HOST = '0.0.0.0'
-    PORT = 12122
-
 app.config.from_object(Config)
 
-app.route('/data/<cmd>/')
-def index(cmd):
+@app.route('/data/<cmd>/', methods=['GET', 'POST'])
+def cmd_data(cmd):
     data=subprocess.getoutput(cmd)
     return jsonify(data)
 
 
-if __name__ == '__name__':
-    app.run()
+app.run()
