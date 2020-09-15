35937bbc4faa840bfca1aad21ade1a12ccf917ba
Ethan <ethan.lv@feixitek.com>
2020-09-15 10:45:20 +0800

    rename file

---
+++ manage/manage.py
@@ -0,0 +1,77 @@
+# coding: utf-8
+
+from flask import Flask, render_template, request, jsonify
+
+import urllib3
+
+IP_LIS=['10.203.1.223', '10.203.1.224']
+
+
+app = Flask(__name__)
+
+class Config(object):
+    DEBUG = True
+
+app.config.from_object(Config)
+
+
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')
+#
+# @app.route('/index')
+# def index():
+#     return render_template('index.html')
+#
+#
+# @app.route('/data_request', methods=['GET', 'POST'])
+# def data_request():
+#     if request.method == 'GET':
+#         data_interface_ip=request.args.get('ip')
+#         data_dic['login_ip']=data_interface_ip
+#         str_login_url=f'http://{data_interface_ip}:12122/login'
+#
+#         #通过urllib3爬取网络资源
+#         http = urllib3.PoolManager()
+#         response = http.request('GET', str_login_url)
+#
+#         #将数据存放在data
+#         if response.status == 200:
+#             data_dic['login_text']=response.data.decode()[1:8]
+#             data_dic['login_status']=response.status
+#         else:
+#             data_dic['login_status']=response.status
+#         print(data_dic)
+#     return 'data'
+#
+# @app.route('/data_response', methods=['GET', 'POST'])
+# def data_response():
+#     return jsonify(data_dic)
+
+
+
+@app.route('/index')
+def index():
+    '''
+    index页面路由
+    :return: index.html
+    '''
+    return render_template('index.html')
+
+@app.route('/get_master_ip', methods=['GET', 'POST'])
+def get_master_ip():
+    '''
+    get_master_ip数据路由
+    :return: T:master ip, F:列表中第一个ip
+    '''
+    for ip in IP_LIS:
+        str_login_url = f'http://{ip}:12122/is_master'
+        http = urllib3.PoolManager()
+        response = http.request('GET', str_login_url)
+        if response.status == 200:
+            if "1" in response.data.decode():
+                return jsonify(ip)
+    return jsonify(IP_LIS[0])
+
+#if __name__ == '__name__' :
+app.run()
--- vplx/vplx.py
+++ vplx/vplx.py
@@ -11,25 +11,34 @@ class Config(object):
 
 app.config.from_object(Config)
 
-def is_master():
+def read_flag_file():
+    '''
+    读取is_master文件
+    :return: 0/1?None
+    '''
     with open('is_master', 'r')as f:
         result=f.read()
     return result
 
-def corss_domain(datadict):
-    response = make_response(jsonify(datadict))
+def corss_domain(data):
+    '''
+    数据跨域
+    :param datadict: 页面返回的数据
+    :return: response
+    '''
+    response = make_response(jsonify(data))
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'OPTIONS,HEAD,GET,POST'
     response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
     return response
 
-@app.route('/login')
-def login():
-    data=is_master()
+@app.route('/is_master')
+def is_master():
+    data=read_flag_file()
     return corss_domain(data)
 
 @app.route('/data/<cmd>/', methods=['GET', 'POST'])
-def cmd_data(cmd):
+def cmd_result(cmd):
     data=subprocess.getoutput(cmd)
     return corss_domain(data)
 
